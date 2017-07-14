71ccec2ee018395c9a5221d07420f507ec21a4fe
Roger Jaffe <rogerjaffe@gmail.com>
2017-07-14 11:10:16 -0700

    Initial commit

---
+++ UsingArrays.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class UsingArrays here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class UsingArrays
+{
+
+  public static void main(String[] args) {
+    int[] grades = {4,2,3,1,3,3,2,0,4,5,5,1,0};
+    
+    // Write the code to compute the average value
+    // DO NOT hard-code the number of items to divide by!
+    // What would you use to find the length of the array?
+    // Use System.out.println() to print the average score to the console
+  }
+}
